9d586018537febea1839e66230241c9973cb6df9
Nir Maoz <maoznir@gmail.com>
2020-04-03 23:25:34 +0300

    Initial commit

---
+++ src/Component.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let name;
+
+  let buttonText = "Button";
+
+  function handleClick() {
+    buttonText = "Button Clicked";
+  }
+</script>
+
+<!-- your code here -->
+<h1>Hello {name}!</h1>
+
+<button on:click={handleClick}>{buttonText}</button>
