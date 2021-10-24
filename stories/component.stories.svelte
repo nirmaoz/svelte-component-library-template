c45e073c4f74b2dc839fec1b9cdcf8009013d45b
Konstantinos Kostarellis <Konstantinos.Kostarellis@gmail.com>
2021-10-24 17:41:59 +0200

    adds an example Story in Svelte Component Style Format

---
+++ stories/component.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Meta, Story } from "@storybook/addon-svelte-csf";
+    import { action } from "@storybook/addon-actions";
+
+    import Component from "../src/index";
+</script>
+
+<Meta title="Component" component={Component} />
+
+<Story name="ComponentStyleFormat-CSF">
+    <Component name="Svelte Story" buttonText="OK" on:click={action('clicked')} />
+</Story>
