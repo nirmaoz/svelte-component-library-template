713dfe02d60e59961a294de7c1ae707cce73659f
Konstantinos Kostarellis <Konstantinos.Kostarellis@gmail.com>
2021-10-24 18:27:57 +0200

    Adds template example

--- stories/component.stories.svelte
+++ stories/component.stories.svelte
@@ -1,12 +1,31 @@
 <script>
-    import { Meta, Story } from "@storybook/addon-svelte-csf";
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
     import { action } from "@storybook/addon-actions";
 
     import Component from "../src/index";
 </script>
 
-<Meta title="Component" component={Component} />
+<Meta
+    title="Component"
+    component={Component}
+    parameters={{
+        actions: { handles: ["click button"] },
+    }}
+/>
+
+<Template let:args>
+    <Component {...args} />
+</Template>
 
 <Story name="ComponentStyleFormat-CSF">
-    <Component name="Svelte Story" buttonText="OK" on:click={action('clicked')} />
+    <Component
+        name="Svelte Story"
+        buttonText="OK"
+        onClick={action("clicked")}
+    />
 </Story>
+
+<Story
+    name="CSF-with-Template"
+    args={{ name: "CSF Template Story", buttonText: "OK" }}
+/>
